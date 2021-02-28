5140904284c4c576a7da8bf8106c3464d2957638
akrigline <a.krigline@gmail.com>
2021-02-28 18:16:38 -0500

    much simpler ts

--- src/devModeTypes.ts
+++ src/devModeTypes.ts
@@ -1,14 +1,13 @@
 import { LogLevel } from './module/constants';
 
 type DebugFlagType = 'boolean' | 'level';
-type DebugFlagValue = { kind: 'boolean'; value: boolean } | { kind: 'level'; value: LogLevel };
 
-type ExcludeKindKey<K> = K extends 'kind' ? never : K;
-type ExcludeKindField<A> = { [K in ExcludeKindKey<keyof A>]: A[K] };
-type ExtractValue<A, T> = A extends { kind: T } ? ExcludeKindField<A> : never;
-
+type DebugFlagValues = {
+  boolean: boolean;
+  level: LogLevel;
+};
 export interface ModuleSpecificDebugFlag<T extends DebugFlagType> {
   packageName: string;
   kind: T;
-  value: ExtractValue<DebugFlagValue, T>['value'];
+  value: DebugFlagValues[T];
 }
